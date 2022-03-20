4adb4fa43040bb2d310eff83d27a7971dc6aaba4
mauro arias <mauro.arias.a@gmail.com>
2022-03-20 06:45:32 -0400

    testing

--- src/org/mauro/git/GitRetriever.groovy
+++ src/org/mauro/git/GitRetriever.groovy
@@ -9,8 +9,8 @@ class GitRetriever implements Serializable {
     def static String gitDstRemote
 
     def public static configGitRep (gitDstRemote) {
-        gitInst.put["gitHub": new GitHub()]
-        gitInst.put["bitBucket": new BitBucket()]
+        gitInst.put("gitHub": new GitHub())
+        gitInst.put("bitBucket": new BitBucket())
         this.gitDstRemote = gitDstRemote
         return gitInst[gitDstRemote]
     }
