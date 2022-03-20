268053fd2235424b13d1d23eeb38aaa7a318b0ae
mauro arias <mauro.arias.a@gmail.com>
2022-03-20 10:14:49 -0400

    testing

--- src/org/mauro/git/BitBucket.groovy
+++ src/org/mauro/git/BitBucket.groovy
@@ -4,7 +4,7 @@ class BitBucket implements Serializable {
 
     def static String baseApiPath = 'https://api.bitbucket.org/2.0/'
 
-    def public static createProjectIfNotExits (steps, projectName) {
+    def public createProjectIfNotExits (steps, projectName) {
         steps.sh "echo 'checking project name: ${projectName}'"
         projectNameKey = "${projectName}".toString().toUpperCase()
         if (!isProjectExits(steps, projectName)) {
@@ -12,40 +12,40 @@ class BitBucket implements Serializable {
         }
     }
 
-    def public static getAuth () {
+    def public getAuth () {
         return "${BIT_BUCKET_CRED_USR}:${BIT_BUCKET_CRED_PSW}"
     }
 
-    def public static getProjectApiPath () {
+    def public getProjectApiPath () {
         return "${baseApiPath}workspaces/${BIT_BUCKET_CRED_USR}/projects/"
     }
 
-    def public static isProjectExits (steps, projectName) {
+    def public isProjectExits (steps, projectName) {
         int status = steps.sh(script: "curl -sLI -w '%{http_code}' --user ${getAuth()} -X GET -H \"Content-Type: application/json\" ${getProjectApiPath()}${projectName} -o /dev/null", returnStdout: true)
         echo "project status code was ${status}"
         return status == 200
     }
 
-    def public static isRepositoryExits (steps, repo) {
+    def public isRepositoryExits (steps, repo) {
         int status = steps.sh(script: "curl -sLI -w '%{http_code}' --user '${getAuth()}' -X GET -H \"Content-Type: application/json\" ${getRepositoryApiPath()}${repo} -o /dev/null", returnStdout: true)
         echo "repository status code was ${status}"
         return status == 200
     }
 
-    def public static getRepositoryApiPath () {
+    def public getRepositoryApiPath () {
         return "${baseApiPath}repositories/${BIT_BUCKET_CRED_USR}/"
     }
 
-    def public static cloneRepo (steps, branche, repoTemplate, template, serviceName) {
+    def public cloneRepo (steps, branche, repoTemplate, template, serviceName) {
         steps.sh "git clone -b ${branch} ${repoTemplate}/${template} ./${serviceName}"
     }
 
-    def public static createRepo (steps, serviceName, projectName) {
+    def public createRepo (steps, serviceName, projectName) {
         projectNameKey = "${projectName}".toString().toUpperCase()
         steps.sh "curl --user ${getAuth()} -X POST -H \"Content-Type: application/json\" --data '{\"scm\":\"git\",\"project\":{\"key\":\"${projectNameKey}\"}}' ${getRepositoryApiPath()}${serviceName}"
     }
 
-    def public static initRepo (steps, serviceName) {
+    def public initRepo (steps, serviceName) {
         sh "git config --global user.email \"$GIT_EMAIL\""
         sh "git config --global user.name \"$GIT_USER\""
         sh "rm -rf .git"
@@ -53,15 +53,15 @@ class BitBucket implements Serializable {
         addRemote(steps, serviceName)
     }
 
-    def public static addRemote (steps, serviceName) {
+    def public addRemote (steps, serviceName) {
         steps.sh "git remote add origin ${getPath()}${serviceName}.git"
     }
 
-    def public static getPath () {
+    def public getPath () {
         return "https://${getAuth()}@bitbucket.org/${BIT_BUCKET_CRED_USR}/"
     }
 
-    def public static commitAndPushRepo (steps) {
+    def public commitAndPushRepo (steps) {
         def branch=Constants.getGitBranch()
         steps.sh "git add -A"
         steps.sh "git commit -m 'first draft from template'"
@@ -86,16 +86,16 @@ class BitBucket implements Serializable {
 
 
 
-    def public static getRepos (projectName) {
+    def public getRepos (projectName) {
         projectNameKey = "${projectName}".toString().toUpperCase()
         return sh(script: "curl --user ${getAuth()} -X GET --url '${getRepositoryApiPath()}?q=project.key%3D+%22${projectNameKey}%22&pagelen=100' --header 'Accept: application/json' | jq -r '.values[] | .name'", returnStdout: true)
     }
 
-    def public static getPathRepo(repository) {
+    def public getPathRepo(repository) {
         return "https://bitbucket.org/${BIT_BUCKET_CRED_USR}/${repository}"
     }
 
-    def public static getRepoOwner() {
+    def public getRepoOwner() {
         return "${BIT_BUCKET_CRED_USR}"
     }
 }
--- src/org/mauro/git/GitHub.groovy
+++ src/org/mauro/git/GitHub.groovy
@@ -6,29 +6,29 @@ class GitHub implements Serializable {
 
     def static String getApiUri ='https://api.github.com/user/repos'
 
-    def public static createProjectIfNotExits (steps, projectName) {
+    def public createProjectIfNotExits (steps, projectName) {
         steps.sh "echo 'ignoring project abstraction, it is not supported in github'"
     }
 
-    def public static isRepositoryExits (steps, projectName) {
+    def public isRepositoryExits (steps, projectName) {
         int status = steps.sh(script: "curl -sLI -w '%{http_code}' -H \"Accept: application/vnd.github.v3+json\" ${getPath(steps)}${projectName} -o /dev/null", returnStdout: true)
         echo "repository status code was ${status}"
         return status == 200
     }
 
-    def public static getPath (steps) {
-        return "https://${GIT_HUB_CRED_USR}:${GIT_HUB_CRED_PSW}@github.com/${GIT_HUB_CRED_USR}/"
+    def public getPath (steps) {
+        return "https://${steps.env.GIT_HUB_CRED_USR}:${steps.env.GIT_HUB_CRED_PSW}@github.com/${steps.env.GIT_HUB_CRED_USR}/"
     }
 
-    def public static cloneRepo (steps, branche, repoTemplate, template, serviceName) {
+    def public cloneRepo (steps, branche, repoTemplate, template, serviceName) {
         steps.sh "git clone -b ${branch} ${repoTemplate}/${template} ./${serviceName}"
     }
 
-    def public static createRepo (steps, serviceName, projectName) {
+    def public createRepo (steps, serviceName, projectName) {
         steps.sh "curl -H \"Authorization: token ${GIT_HUB_TOKEN}\" --data '{\"name\":\"${serviceName}\"}' ${getApiUri}"
     }
 
-    def public static initRepo (steps, serviceName) {
+    def public initRepo (steps, serviceName) {
         sh "git config --global user.email \"$GIT_EMAIL\""
         sh "git config --global user.name \"$GIT_USER\""
         sh "rm -rf .git"
@@ -36,11 +36,11 @@ class GitHub implements Serializable {
         addRemote(steps, serviceName)
     }
 
-    def public static addRemote (steps, serviceName) {
+    def public addRemote (steps, serviceName) {
         steps.sh "git remote add origin ${getPath(steps)}${serviceName}.git"
     }
 
-    def public static commitAndPushRepo (steps) {
+    def public commitAndPushRepo (steps) {
         def branch=Constants.getGitBranch()
         steps.sh "git add -A"
         steps.sh "git commit -m 'first draft from template'"
@@ -58,15 +58,15 @@ class GitHub implements Serializable {
 
 
 
-    def public static getRepos () {
+    def public getRepos () {
         return sh(script: "curl -H \"Authorization: token ${GIT_HUB_TOKEN}\" -X GET ${getApiUri} | jq -r '.[] | .name'", returnStdout: true)
     }
 
-    def public static getPathRepo(repository) {
+    def public getPathRepo(repository) {
         return "https://github.com/${GIT_HUB_USER}/${repository}"
     }
 
-    def public static getRepoOwner() {
+    def public getRepoOwner() {
         return "${GIT_HUB_USER}"
     }
 }
